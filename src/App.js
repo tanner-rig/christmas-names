31499a7a3f09232a54aa0775a6704c569579839b
Tanner Rigby <tannerrigby@Tanners-MacBook-Pro.local>
2018-10-14 20:38:06 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
+  state = {
+    names: ["Jessica", "Brad", "Dixie", "Tanner", "Kylee", "Colton"]
+  };
   render() {
+    const { names } = this.state;
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="list">
+          <div className="list-item">{`${names[0]} has ${names[4]}`}</div>
+          <div className="list-item">{`${names[1]} has ${names[5]}`}</div>
+          <div className="list-item">{`${names[2]} has ${names[3]}`}</div>
+          <div className="list-item">{`${names[3]} has ${names[0]}`}</div>
+          <div className="list-item">{`${names[4]} has ${names[1]}`}</div>
+          <div className="list-item">{`${names[5]} has ${names[2]}`}</div>
+        </div>
       </div>
     );
   }
